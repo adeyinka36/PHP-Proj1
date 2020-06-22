29c329bdad23f69ada9133c37c1df6fcaa415ef4
Adeyinka Giwa <adeyinka36@gmail.com>
2020-06-22 12:01:19 +0100

    completed for review

---
+++ inc/functions.php
@@ -0,0 +1,59 @@
+<?php
+// PHP - Random Quote Generator
+$quotes=[
+    [
+       "quote"=>"The greatest glory in living lies not in never falling, but in rising every time we fall.",
+       "source"=>"Nelson Mandela"
+    ],
+    [
+        "quote"=>"The way to get started is to quit talking and begin doing.",
+        "source"=>"Walt Disney"
+    ],
+    [
+        "quote"=>"Life is what happens when you're busy making other plans.",
+        "source"=>"John Lenon",
+        "year"=>"1983"
+    ],
+    [
+        "quote"=>"Keep your friends cose and your enemies closer",
+        "source"=>"Robert Deniro",
+        "citation"=>"The GodFather",
+        "year"=>"1972"
+    ],
+    [
+        "quote"=>"I'll be back",
+        "source"=>"Arnold Schwarzenegger",
+        "citation"=>"The Terminator",
+        "year"=>"1984"
+    ]
+    ];
+
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+function getRandomQuote($array){
+    $randomNum=rand(0,count($array)-1);
+    return $array[$randomNum];
+    
+}
+
+
+// Create the printQuote funtion and name it printQuote
+function printQuote($array){
+  $empty="";
+  $result= getRandomQuote($array);
+  $empty.="<p class=quote>".$result["quote"]."</p>";
+  $empty.="<p class=source>".$result["source"]."</p>";
+  if(isset($result["citation"])){
+    $empty.="<span class=citation>".$result["citation"]."</span>";
+  }
+  if(isset($result["year"])){
+      $empty.="<span class=year>".$result["year"]."</span>";
+  }
+
+ 
+  return $empty;
+}
+
+?>
